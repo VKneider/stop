7885b4d7d37ea93b9e3dc103c13be9d906ddcfd7
Victor Kneider <victorkneider@gmail.com>
2023-03-03 00:08:28 -0400

    feat: added adaptive design for landing page

--- src/public/landingPage/main.js
+++ src/public/landingPage/main.js
@@ -1,8 +1,6 @@
 io();
 import Fetch from "../Slice/fetch.js";
 
-const fetchModule = new Fetch("https://pokeapi.co/api/v2/pokemon", 10000);
-const fetchFake = new Fetch("https://fakestoreapi.com", 10000);
 
 let init = async () => {
     let NB = await slice.getInstance("complexNavbar", {
@@ -24,18 +22,6 @@ let init = async () => {
     let cardMultiplayer = await slice.getInstance("Card", { title: "Multiplayer", text: "You can play with your friends, the minimum ammount of players required to create a Room is 3. Invite your friends to play and have fun!", icon: "fa fa-users", color: "#2B3467" });
     let cardContext = await slice.getInstance("Card", { title: "Think rapidly", text: "The game consists of input fields where you have to write down words depending on the category and all of them must start with the letter chosen in each round.", icon: "fa fa-font", color: "#2B3467" });
 
-    
-    /*
-    let input = document.createElement("input");
-    input.setAttribute("type", "text");
-    input.setAttribute("placeholder", "Enter your name");
-    input.setAttribute("id", "name");
-
-    let input2 = document.createElement("input");
-    input2.setAttribute("type", "text");
-    input2.setAttribute("placeholder", "Enter your name");
-    input2.setAttribute("id", "xd");
-    */
     
     let path = "./landingPage/img/stock"
     let carousel = await slice.getInstance("img-carousel", {id:"main-carousel", jumpInterval:2 ,motion:"fw", images:[`${path}1.jpg`, `${path}2.jpg`, `${path}3.jpeg`,`${path}4.jpg` ]});
@@ -60,7 +46,6 @@ let init = async () => {
     document.getElementById("howtoplay").appendChild(cardContainer);
 
     
-
     //https://colorhunt.co/palette/2b3467bad7e9fcffe7eb455f
 };
 init();
