d9a040844f046e1dd6e9adb3bae5c009901ee780
Victor Kneider <victorkneider@gmail.com>
2023-03-03 00:10:34 -0400

    feat: added register endpoint

--- src/server.js
+++ src/server.js
@@ -4,17 +4,38 @@ import dotenv from "dotenv";
 import {dirname} from "path";
 import path from "path";
 import { fileURLToPath } from 'url';
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
 dotenv.config({ path: "./env/local.env" });
+import sess from "./components/Session/Session.js";
+import session from "express-session";
+
 const app = express();
 const bodyParser = express.json();
-
 app.set("port", process.env.PORT || 3000);
 app.use(cors());
 app.use(bodyParser);
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(session({
+    secret: "secret",
+    resave:false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000*60*60*24,
+        httpOnly:true,
+        secure: false
+    }
+}));
+
+
+import profileRouter from "./Routes/profileRouter.js";
+
+
+
+
+
+
+
 
 const server = app.listen(app.get("port"), "", (req,res) => {
     console.log("Server is running on port: " + app.get("port"));
@@ -28,5 +49,11 @@ app.get("/", (req, res) => {
 app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "LoginRegister", "index.html"))
 });
+
+app.post("/login", sess.login);
+
+app.post("/register", (req,res) => sess.register(req,res));
+
+app.use("/profile", profileRouter);
 
 export default server;
