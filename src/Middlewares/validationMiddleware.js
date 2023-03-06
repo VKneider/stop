ccb262c85df80e53ecc48b3a916e0e2bc988bd69
Victor Kneider <victorkneider@gmail.com>
2023-03-06 03:34:18 -0400

    feat: add validation middleware

--- src/Middlewares/validationMiddleware.js
+++ src/Middlewares/validationMiddleware.js
@@ -1,13 +1,13 @@
-const validation = (schema) => async (req, res, next) => {{ 
 
+const validationYup = (schema) => async (req, res, next) => {{ 
     const body = req.body;
 
     try {
         await schema.validate(body);
         next();
     } catch (error) {
-        return res.status(400).json({ error });
+        return res.status(200).json({ error });
     }
 }}
 
-export default validation;
+export default validationYup;
