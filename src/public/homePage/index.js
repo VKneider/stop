a742811c7dd8b0cb0dd7d37e285cdae99ce11b67
Victor Kneider <victorkneider@gmail.com>
2023-03-08 01:49:16 -0400

    feat: add homePage view

--- src/public/homePage/index.js
+++ src/public/homePage/index.js
@@ -1,17 +1,54 @@
+import mainFetch from "../Slice/fetch.js";
+
+
 let init = async () => {
     
     let NB = await slice.getInstance("complexNavbar", {
         logo: "Stoppery",
         id: "myNavbar",
         sections: [
-            { id: "#description", text: "Description" },
-            { id: "#howtoplay", text: "How to Play" },
-            { id: "#aboutus", text: "About Us" }
+            { link: "#profile", text: "My Profile", id: "profile" },
+            { link: "#profile", text: "Log Out", id: "logOut" }
         ]
     });
 
 
+    let btnContainer = document.getElementById("btnContainer");
+
+    
+    let playBtn = await slice.getInstance("Button", { value: "Play Now", style: { width: "50%", margin: "50px", background: "#2B3467" } });
+    let createRoomBtn = await slice.getInstance("Button", { value: "Create Room", style: { width: "50%", margin: "50px", background: "#BAD7E9", color: "black" } });
+    let joinRoomBtn = await slice.getInstance("Button", { value: "Join Room", style: { width: "50%", margin: "50px", background: "#EB455F" } });
+    
+    btnContainer.appendChild(playBtn);
+    btnContainer.appendChild(createRoomBtn);
+    btnContainer.appendChild(joinRoomBtn);
+
+    let waitingModal = await slice.getInstance("WaitingModal");
+
+    joinRoomBtn.addEventListener("click", () => {
+        document.body.appendChild(waitingModal);
+    });
+
+    NB.getElement("logOut").addEventListener("click", async (e) => {
+        e.preventDefault();
+
+        let call = await mainFetch.request("POST", {}, "/logout");
+        console.log(call)
+        if (call.status == 200) {
+            window.location.href = "http://localhost:3003";
+        }
+
+    });
+
+    
+    
     document.body.appendChild(NB);
 }
 
 init();
+
+
+
+		
+		
